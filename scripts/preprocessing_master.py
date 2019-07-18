d73eb55a103df33b03afb97ec34358b6eef36660
Ömer <omer.ozumerzifon@gmail.com>
2019-07-18 18:21:51 +0200

    Zeitumstellung Problem gelöst

--- scripts/preprocessing_master.py
+++ scripts/preprocessing_master.py
@@ -15,12 +15,17 @@ def preprocess_Rawdata(projectPath='/Users/ozumerzifon/Desktop/BDA-ömer_aktuell
                                      how='outer')
     df = df.sort_values('date')
 
+    # Delete empty rows of export due to time change --> Rows after 02-06-2019 stay in the df
+    df = df.drop(df[(df['date'] < pd.to_datetime('02-06-2019 00:00:00', format='%d-%m-%Y %H:%M:%S')) & (
+        df.iloc[:, 33:43].isnull().any(axis=1))].index).reset_index(drop=True)
+
+    # Export to csv
     df.to_csv(projectPath + 'data/master_df.csv', sep=';', decimal=',', index=False)
 
     return df
 
 
 def import_masterDataFrame(projectPath='/Users/ozumerzifon/Desktop/BDA-ömer_aktuell/', path='data/master_df.csv'):
-    df_master = pd.read_csv(projectPath + path, sep=';', decimal=',')
+    df_master = pd.read_csv(projectPath + path, sep=';', decimal=',', parse_dates=['date'])
 
     return df_master
